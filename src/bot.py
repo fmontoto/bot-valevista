560982f9ad199942b42caaa2d805b0f1c1cb9da0
francisco <fmontotomonroy@gmail.com>
2016-11-18 04:26:56 +0000

    merge

--- src/bot.py
+++ src/bot.py
@@ -64,7 +64,7 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, msg))
 
     dp.add_error_handler(error)
-    updater.start_webhook(listen="0.0.0.0", port=443, url_path="/simplebot")
+    updater.start_webhook(listen="0.0.0.0", port=443, url_path="/bot-valevista")
     updater.idle()
 
 if __name__ == "__main__":
